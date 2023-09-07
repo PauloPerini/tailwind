150c9118b97cef4c152379e2d6087c8eb2727fa2
Paulo Perini <perini.paulopc@gmail.com>
2023-09-07 11:06:07 -0300

    att

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        azul: {
+          claro:'#c5dfff',
+          escuro:'#061e3c',
+          hover:'#1057b0'
+        },
+      },
+      fontFamily:{
+        inter: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
